77fc1e6f47c80acc7ffc8897088645fee41d5b86
abubakarshaikh <atifa2019@gmail.com>
2022-05-08 00:34:20 +0500

    first push

---
+++ lib/apiservice/ApiHeader.dart
@@ -0,0 +1,19 @@
+import 'package:dio/dio.dart';
+import 'package:mealup_driver/util/constants.dart';
+import 'package:mealup_driver/util/preferenceutils.dart';
+
+class ApiHeader {
+  Dio dioData() {
+    final dio = Dio();
+
+    dio.options.headers["Authorization"] = "Bearer" +
+        "  " + PreferenceUtils.getString(Constants.headertoken); // config your dio headers globally
+    dio.options.headers["Accept"] =
+        "application/json"; // config your dio headers globally
+    // dio.options.headers["Content-Type"] = "application/x-www-form-urlencoded";
+    dio.options.followRedirects = false;
+
+
+    return dio;
+  }
+}
